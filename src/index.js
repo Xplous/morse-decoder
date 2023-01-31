9f77aea14751a3e21a20d26ad547bd9ab90deb8a
Nikita <68853033+Xplous@users.noreply.github.com>
2023-01-31 23:26:32 +0300

    Complete tusk :+1:

--- src/index.js
+++ src/index.js
@@ -35,12 +35,22 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    
 };
-
 function decode(expr) {
-    // write your solution here
-}
+  let decodes = '';
+  let key = [];
 
+  for (let i = 0; i < expr.length; i += 10) {
+      key = expr.slice(i, i + 10).replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+      if (key === '**********') {
+          decodes += ' ';
+      } else {
+        decodes += MORSE_TABLE[key];
+      }
+  }
+  return decodes;
+}
 module.exports = {
     decode
 }
